65ea7087f64931bbe168d959f8ff318c3418b029
wyt <yutongwang2023@lzu.edu.cn>
2024-11-11 22:10:01 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,11 @@
+Page({
+  data: {
+    motto: '你好，小程序！',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl')
+  },
+  onLoad() {
+    
+  }
+}) 
